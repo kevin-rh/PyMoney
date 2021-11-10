3c7f4b12d925fcaccdbe80d9dcf705827c70ac04
kevin-rh <86484810+kevin-rh@users.noreply.github.com>
2021-11-10 14:04:45 +0800

    Update: Multiple Records Input
    Enabling multiple records input and store it in list-tuple data structure.

--- pymoney.py
+++ pymoney.py
@@ -1,8 +1,12 @@
-money = input("How much money do you have? ")
-
+money = int(input("How much money do you have? "))
 record = input("Add an expense or income record with description and an amount:\n")
-name,price = record.split()
 
-total = int(money) + int(price)
+list_record = [(tuple([data.split()[0], int(data.split()[1])])) for data in record.split(',')]
+#print (list_record)
 
-print("Now you have %d dollars." %total)
+print('Here\'s your expense and income records:')
+for data in list_record:
+  print(f'{data[0]:s} {data[1]:d}')
+  money += data[1]
+
+print("Now you have %d dollars." %money)
