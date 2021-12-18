774376d7e19fc58bf1d4e885cab0fd4d73bbb0ab
kevin-rh <86484810+kevin-rh@users.noreply.github.com>
2021-12-19 01:51:48 +0800

    Update: Categorized Records
    Adding a new feature: Categorize.
    View, find, add, and delete functions got modified due to adding new features.
    I/O from files got modified due to adding new features.
    Adding a docstring to every functions.

--- pymoney.py
+++ pymoney.py
@@ -1,11 +1,20 @@
 # Headers
 import sys
+print_Category = "Category"
 print_Desc = "Description"
 print_Amount = "Amount"
-cmd_set = ("add", "view", "delete", "exit")
+cmd_set = (
+    "add", 
+    "view", 
+    "delete", 
+    "view categories", 
+    "find", 
+    "exit",
+)
 
 # Program Functions
 def input_money():
+    """Insert the amount money at the initialization."""
     money=0
     # Loop until the money input format is correct
     print("Hi, new user!")
@@ -19,6 +28,9 @@ def input_money():
             return money
 
 def initialize():
+    """Initialize this program at start.
+    Read data in file if available, else request initialize new data.
+    """
     money = 0
     record = []
     # Read Data from a File
@@ -36,51 +48,75 @@ def initialize():
                 money = input_money()
             else:                   # Read data to the record variable
                 print("Welcome back!\n")
-                record = [ tuple([data.split(', ')[0],int(data.split(', ')[1])]) for data in fh.readlines()]
+                record = [tuple([data.split(', ')[1], int(data.split(', ')[2]), data.split(', ')[0]]) for data in fh.readlines()]
+                # record_pair_list = [(catg+', '+name+', '+str(amt)) for name,amt,catg in record]
     except FileNotFoundError:       # Exception: File did not exist
         money = input_money()
     return money, record
 
-def add(money, record):
+def add(money, record, categories):
+    """Add new record.
+
+    Keyword arguments:
+    money       -- the total money
+    record      -- the entire record list
+    categories  -- the entire category list
+    """
     while(True):
         try:
             new_record = input("Add an expense or income record with description and an amount:\n")
             assert new_record != 'q'
-            desc, amount = new_record.split()
+            new_record = new_record.split()
+            [category, desc, amount] = new_record
+            if not is_category_valid(categories, category):
+                categories = add_categories(categories, category)
         except AssertionError:
-            print("Cancle Addition\n")
-            return money, record
+            print("Cancel Add Fucntion\n")
+            return money, record, categories
         except ValueError: # Exception if can not split into two (desc, amount)
-            print("Invalid format. Expect 2 members: <description> <amount>. e.g.: breakfast -50\n")
+            print("Invalid format.\nFormat: <category> <description> <amount>. e.g.: food breakfast -50\n")
         else:
             try:
                 amount_int = int(amount)
             except ValueError:
-                print("Invalid format. Amount should be an integer.\n")
+                print("Invalid format. <amount> should be an integer.\n")
             else:
-                record.append(tuple([desc,amount_int]))
+                record.append(tuple([desc, amount_int, category]))
                 money += amount_int
                 print()
-                return money, record
+                return money, record, categories
 
-def view(money, record):
-    print("Here\'s your expense and income records:")
-    print(f"{print_Desc:<25s} {print_Amount:^8s}")
-    print("="*25+" "+"="*8)
+def view(money, record, categories): #TODO: Check the categories part
+    """View record.
+
+    Keyword arguments:
+    money       -- the total money
+    record      -- the entire record list
+    categories  -- the entire category list
+    """
+    print(f"Here\'s your expense and income records:")
+    print(f"{print_Category:<25s} {print_Desc:<25s} {print_Amount:^8s}")
+    print("="*25 +" "+ "="*25 + " "+"="*8)
     for data in record:
-        print(f'{data[0]:<25s} {data[1]:>8d}')
-
-    print("="*25+" "+"="*8)
+        print(f'{data[2]:<25s} {data[0]:<25s} {data[1]:>8d}')
+    print("="*25 +" "+ "="*25 + " "+"="*8)
     print("Now you have %d dollars.\n" %money)
     pass
 
-def delete(money, record):
+def delete(money, record, categories): #TODO: Bind with categories
+    """Delete record.
+
+    Keyword arguments:
+    money       -- the total money
+    record      -- the entire record list
+    categories  -- the entire category list
+    """
     while(True):
         try:
             delete = input("Which record do you want to delete? ").split()
             assert delete != 'q'
         except AssertionError:
-            print("Cancel Deletion\n")
+            print("Cancel Deletion Function\n")
             break
         else:
             try:
@@ -141,17 +177,33 @@ def delete(money, record):
     print()
     return money, record
 
-def save(money, record):
+def save(money, record, categories):
+    """Write record inside a file.
+
+    Keyword arguments:
+    money       -- the total money
+    record      -- the entire record list
+    categories  -- the entire category list
+    """
     # DEBUG
     #print(record)
-    record_pair_list = [(i+', '+str(j)) for i,j in record]
+    record_pair_list = [(catg+', '+name+', '+str(amt)) for name,amt,catg in record]
     record_line_list = '\n'.join(record_pair_list)
-
-    fh = open('records.txt',mode='w')
-    fh.write(f'{money}\n')              # write money at first line
-    fh.writelines(record_line_list)     # write record list
-    fh.close()
-    print("File sucessfully saved.\n")
+    
+    # Write the data
+    try:
+        with open("records.txt","w") as fh:
+            try:
+                fh.write(f'{money}\n')              # write money at first line
+                fh.writelines(record_line_list)     # write record list
+                fh.close()
+            except:      # Exception: File first line not an Integer
+                print("Fail to write the recorded file.\n")
+            else:                   # Read data to the record variable
+                print("File sucessfully saved.\n")
+    except FileNotFoundError:       # Exception: File did not exist
+        print("File did not existed. Failed to write data to the file!\n")
+    
     while True:
         try:
             confirm = input("Exit (y/n)? ")
@@ -163,6 +215,9 @@ def save(money, record):
             return (confirm =='y')
 
 def input_command():
+    """Input command error control.
+    Control when a command in the intreperter is correct or not.
+    """
     while(True):
         try:
             cmd = input("What do you want to do ("+" / ".join(cmd_set) +")? ")
@@ -173,18 +228,130 @@ def input_command():
             print()
             return cmd
 
+
+def find(records, categories):
+    """Find record by categories.
+
+    Keyword arguments:
+    record      -- the entire record list
+    categories  -- the entire category list
+    """
+    while(True):
+        try:
+            category = input("Which category do you want to find? ")
+            assert category != 'q'
+        except AssertionError:
+            print("Cancel Find Function\n")
+            break
+        else:
+            try:
+                if category != "all": assert is_category_valid(categories, category) == True
+            except AssertionError:
+                print(f"Category {category:s} is not available!\n")
+            except ValueError:
+                print(f"Category should be a string.\n")
+            else:
+                #TODO: print the filtered records
+                amounts = 0
+                print(f"Here\'s your { category if category!='all' else 'expense and income'} records:")
+                # Headers
+                print(f"{print_Category:<25s} {print_Desc:<25s} {print_Amount:^8s}")
+                print("="*25 +" "+ "="*25 + " "+"="*8)
+                
+                for data in filter(lambda rec: rec[2] in find_subcategories(categories, category) if category != 'all' else initialize_categories(), records):
+                    print(f'{data[2]:<25s} {data[0]:<25s} {data[1]:>8d}')
+                    amounts += data[1]
+                print("="*25 +" "+ "="*25 + " "+"="*8)
+                print(f"The total amount above is {amounts:d} dollars.\n")
+                break
+    pass
+
+# Categories Function
+def initialize_categories ():
+    """Return initialized all category in form of a list."""
+    return ['expense', ['food', ['meal', 'snack', 'drink'], 'transportation', ['bus', 'railway']], 'income', ['salary', 'bonus']]
+
+def find_subcategories(categories, category):
+    """Return all subcategories of a category as a list.
+    Return empty list [] if no category is found.
+
+    Keyword arguments:
+    categories  -- the entire category list
+    category    -- the searched category
+    """
+    if type(categories) == list:
+        for i, v in enumerate(categories):
+            p = find_subcategories(v, category)
+            if p == True:
+                return flatten(categories[i:i+2 if (i+2) <= len(categories) else i+1])
+            if p != []:
+                return p
+    return categories == category or []
+
+def is_category_valid(categories, category):
+    """Return True if a category is exist inside the category list.
+    Otherwise, return False.
+
+    Keyword arguments:
+    categories  -- the entire category list
+    category    -- the searched category
+    """
+    if type(category) in {list, tuple}: # Only has one value
+        for cat in category:
+            return is_category_valid(categories, cat)
+    
+    return False if find_subcategories(categories, category) == [] else True
+
+def view_categories(categories, depth=0):
+    """Print all the whole category list.
+
+    Keyword arguments:
+    categories  -- the entire category list
+    depth       -- the depth occur recursively (default = 0)
+    """
+    if depth == 0: print("Category: ")
+    for i, atom in enumerate(categories):
+        if type(atom) not in {list, tuple}:
+            print(' '*4*depth + '- ' + atom)
+        else :
+            view_categories(atom, depth+1)
+        # depth+'.'+str(i+1) if depth else str(i+1)   
+    if depth ==0: print()
+      
+def flatten(L):
+    """Return a flat list that contains all element in the nested list L.
+    
+    For example, flatten([1, 2, [3, [4], 5]]) returns [1, 2, 3, 4, 5].
+    
+    Keyword arguments:
+    L   -- recursive structured list
+    """
+    if type(L) not in {list, tuple}:
+        return [L]
+    flat = []
+    for atom in L:
+        flat.extend(flatten(atom))
+    return flat
+
 # Main Program
 money, list_record = initialize()
+categories = initialize_categories()
 # Input Command
 while True:
     cmd = input_command()
     if cmd == "add":
-        money, list_record = add(money, list_record)
+        money, list_record, categories = add(money, list_record, categories)
     elif cmd == "view":
-       view(money, list_record)
+       view(money, list_record, categories)
     elif cmd == "delete":
-        money, list_record = delete(money, list_record)
+        money, list_record, categories = delete(money, list_record, categories)
+    elif cmd == "view categories":
+        view_categories(categories)
+    elif cmd == "find":
+        find(list_record, categories)
     elif cmd == "exit":
-        if save(money, list_record):
+        if save(money, list_record, categories):
             break
+        else :
+            print("Something went wrong, while writing the data to the file.")
 print("See you, thank you!")
